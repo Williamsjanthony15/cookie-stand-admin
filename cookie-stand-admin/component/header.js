4b7cd17629631cf856aee3faea6299d43785795f
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-10-05 23:27:01 -0500

    log in log out button there. Working, Need paired with Database

--- cookie-stand-admin/component/header.js
+++ cookie-stand-admin/component/header.js
@@ -1,9 +1,30 @@
 import Link from 'next/link'
+import { useAuth } from '../contexts/auth'
+
 export default function Header(props) {
+  const { login, logout } = useAuth();
     return (
         <header className="flex items-center justify-between p-4 bg-green-500 text-black-100">
             <h1 className="text-4xl">Cookie Stand Admin</h1>
             <Link href='/overview'><a>Next</a></Link>
+            <button
+              onClick={() =>
+                login(
+                  process.env.NEXT_PUBLIC_USERNAME,
+                  process.env.NEXT_PUBLIC_PASSWORD
+                )
+              }
+              className=" p-2 text-white bg-gray-500 rounded"
+            >
+              Login
+            </button>
+            <button
+              onClick={logout}
+              className="p-2 text-white bg-gray-500 rounded"
+            >
+              Logout
+            </button>
+
         </header>
     )
 }
--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -1,33 +1,50 @@
 import Head from "next/head";
-import Header from '../component/header.js'
-import Main from '../component/main.js'
+import Header from "../component/header.js";
+import Main from "../component/main.js";
+import { useAuth } from '../contexts/auth'
 // import CreateForm from '../component/createForm.js'
 // import ReportTable from "../component/reportTable.js";
 // import Footer from '../component/footer.js'
 
 export default function Home() {
+  const { user, logout } = useAuth();
   return (
-    <div className='bg-green-300'>
+    <div className="bg-green-300">
       <Head>
         <title>Cookie Stand Admin</title>
-        <link rel='icon' href='/faviron.ico' />
+        <link rel="icon" href="/faviron.ico" />
       </Head>
+      <main className="p-4 space-y-8 text-center">
+        <h1 className="text-4xl">Getting Data</h1>
+        {user ? (
+          <>
+            <h2>Welcome!{user.username}</h2>
+            {/* <button
+              onClick={logout}
+              className="p-2 text-white bg-gray-500 rounded"
+            >
+              Logout
+            </button> */}
+            <Main />
+          </>
+        ) : (
+          <>
+            <h2>Need to log in</h2>
+            {/* <button
+              onClick={() =>
+                login(
+                  process.env.NEXT_PUBLIC_USERNAME,
+                  process.env.NEXT_PUBLIC_PASSWORD
+                )
+              }
+              className="p-2 text-white bg-gray-500 rounded"
+            >
+              Login
+            </button> */}
+          </>
+        )}
+      </main>
       <Header />
-      <Main />
     </div>
-  )
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
